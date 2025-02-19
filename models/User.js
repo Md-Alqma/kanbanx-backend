6ee2d676ca54e8e0d8dd9348492708d1c31fa319
Md-Alqma <mdalqma7@gmail.com>
2025-02-19 23:20:29 +0530

    added boards to the user schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
   password: String,
+  boards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Board" }], // A User has multiple boards
 });
 
 module.exports = mongoose.model("User", userSchema);
